aa909711c54fa2a9f10aee7ef408d32b5486fbd7
EugeniuZaharia <78173175+EugeniuZaharia@users.noreply.github.com>
2021-04-14 18:51:58 +0200

    Tests

--- src/components/Gege.jsx
+++ src/components/Gege.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+
+const input = ({
+    input: "",
+} return ()
 
 function Gege() {
+axios.get(`https://www.rijksmuseum.nl/api/nl/collection?key=eQKm0Vx6&q=62672`})
+.then(function response)}
+
+
     return (
         <div>
-            <p>gege</p>
+            <p>Gege</p>
         </div>
     )
 }
 
 export default Gege
+
+
+
