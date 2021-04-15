8fe3c08e61830d3ee3c0c797934c31a47e43c006
Maya Imhoff <mayelyimhoff@gmail.com>
2021-04-15 12:05:47 +0200

    api iteration wroks, can display multiple results

--- src/components/Maya.jsx
+++ src/components/Maya.jsx
@@ -1,48 +1,46 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "../css/Maya.css"
 
-
-
-  // we display only 10 items, and appear on clicking of category button
 function Maya() {
-    // const [artTitle, setArtTitle] = useState("");
-    // const [artMaker, setArtMaker] = useState("");
-    // const [artImage, setArtImage] = useState("");
-    // https://www.rijksmuseum.nl/api/nl/collection?key=eQKm0Vx6&culture=en&involvedMaker=Charles%20Martin&f.dating.period=20&imgonly=True
+    const [art, setArt] = useState(null);
 
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        axios
-            .get("https://www.rijksmuseum.nl/api/nl/collection?key=eQKm0Vx6&culture=en&involvedMaker=Charles%20Martin&f.dating.period=20&imgonly=True"
-            )
-            .then((res) => res.data)
-            .then((data) => {
-                setData(data);
-                console.log(data);
-            // .catch((err) => {
-            //   console.error(err.response.data);
-            });
-      }, []);
-
-
-    return (
-        <>
-        
-            {data.map((item) => {
-            <div>
-                <img src={item.artObjects.webImage.url} style={{ width: "200px" }} />
-                <div>
-                <h4>{item.artObjects.title} </h4>
-                </div>
-                <div>
-                <p>{item.artObjects.principalOrFirstMaker} </p>
-                </div>
-            </div>
-        })}
+    const artData = async () => {
+      try {
+      
+        const res = await axios.get("https://www.rijksmuseum.nl/api/nl/collection?key=eQKm0Vx6&involvedMaker=Maurice+Leroy");
+        const artArray = [];
+  
+        res.data.artObjects.map((el) => artArray.push([el.webImage.url, el.title, el.principalOrFirstMaker ]));
+        console.log(artArray)
+        setArt(artArray);
+  
        
-        </>
-    )
-}
+      } catch (error) {
+        console.log(error);
+      }
+    }; 
+  
+    useEffect(() => {
+      artData();
+    }, []);
+  
+    return (
+      <div>
+       {art &&
+            art.map((artItem, index) => {
+          
+              return (
+                <div className="card" key={index}>
+                <img src={artItem[0]} style={{ width: "50%" }} alt="" />
+                <h1>Artist: {artItem[2]}</h1>  
+                <p>Title: {artItem[1]}</p>
+                </div>
+              );
+            })}
+      </div>
+    );
+  }
+  
 
 export default Maya
