17957e4eff319da4bb7e9a2645fe2a3b3c186dda
Maya Imhoff <mayelyimhoff@gmail.com>
2021-04-15 18:04:27 +0200

    added intro text to category page, styled buttons

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -13,6 +13,10 @@ function Categories() {
 
   return (
     <>
+    <div className="intro">
+      <h1>Our hottest five Art Collections, chosen by us for you.</h1>
+      <h2>click a button and find your favs!</h2>
+    </div>
     
       <Router>
           <div className="button-row">
