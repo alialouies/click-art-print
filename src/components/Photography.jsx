53852306de0ad3ba49d42f6fd4e442e8acda6106
alialouies <makarona.aliki@gmail.com>
2021-04-16 11:16:14 +0200

    adding css

--- src/components/Photography.jsx
+++ src/components/Photography.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
-// import "../css/PrintArt.css"
+import "../css/PrintArt.css"
 
 function Photography() {
   const [data, setData] = useState([]);
