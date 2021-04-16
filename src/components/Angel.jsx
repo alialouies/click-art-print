4c338b7e9073b926d06ab66326239264e6e4ec09
Angel Sámano Hamud <Angelsam@air-de-angel.localdomain>
2021-04-16 08:47:51 +0200

    Add Api

--- src/components/Angel.jsx
+++ src/components/Angel.jsx
@@ -1,34 +1,55 @@
 import React, {useEffect, useState} from "react"
 import axios from "axios"; 
-import DisplayDataAngel from "./AngelComponet"
-
-function Angel() {
-const [department, setDepartment] = useState("");
-
-const getDataDepartment = () => {
-    axios
-    .get("https://collectionapi.metmuseum.org/public/collection/v1/departments")
-    .then((response) => {
-        console.log(response.data.departments[0].displayName); 
-        setDepartment(response.data.departments[3].displayName); 
-        
-    })
-}; 
 
 
+function PrintObject() {
+    const [object, setObject] = useState(null);
+  
+    const objectData = async () => {
+      try {
+      
+        const res = await axios.get("https://www.rijksmuseum.nl/en/search?p=1&ps=12&f.principalMakers.name.sort=George%20Barbier&st=Objects&ii=0");
+        const objectArray = [];
+  
+        res.data.artObjects.map((el) => objectArray.push([el.webImage.url, el.title, el.principalOrFirstMaker ]));
+        console.log(objectArray)
+        setObject(objectArray);
+  
+       
+      } catch (error) {
+        console.log(error);
+      }
+    }; 
+  
+    useEffect(() => {
+      objectData();
+    }, []);
 
-useEffect (()=>{
-    getDataDepartment();},[]
-)
+     const randomPrice = () => {
+      let price = ["14.99", "19.99", "29.99", "24,99"];
+      let random = price[Math.floor(Math.random() * price.length)];
+      return random;
+    
+    }
 
     return (
-        <div>
-            <p>10 Pictures Angel</p>
-           <p> {department} </p> 
-            <button type="button" onClick={getDataDepartment}>Get Data</button>
-
-        </div>
-    )
-}
+      <div>
+       {object &&
+      //  art.slice(2, 12).map((artItem, index) => {
+            object.map((artItem, index) => {
+          
+              return (
+                <div className="card" key={index}>
+                <img src={artItem[0]} style={{ width: "90%" }} alt="" />
+                <h5>Artist: {artItem[2]}</h5>  
+                <p>Title: {artItem[1]}</p>
+                <h5>Price: {randomPrice()} €</h5>
+                </div>
+              );
+            })}
+      </div>
+    );
+  }
+  
 
 export default Angel
