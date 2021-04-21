a34c2ce47d61271ddfc815c26f9e51609abf51b7
EugeniuZaharia <78173175+EugeniuZaharia@users.noreply.github.com>
2021-04-21 12:57:00 +0200

    first step

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from './components/pages/Home';
-import Shop from './components/pages/Shop';
-import Cart from './components/pages/Cart';
-import Navbar from './components/Navbar';
-
+import Home from "./components/pages/Home";
+import Shop from "./components/pages/Shop";
+import Cart from "./components/pages/Cart";
+import Navbar from "./components/Navbar";
+import React, { useState } from "react";
+import Pictures from "./components/Pictures";
 
 function App() {
+  //On click - Chage state Shopping Cart
+  const [cart, setCart] = React.useState([]);
+
+  // => to add the item to cart
+  const addCart = (artProduct) => {
+    console.log(artProduct);
+    setCart(artProduct);
+  };
+
+  //
+
   return (
     <>
       <Router>
-        <Navbar/>
-        <Switch> 
-          <Route exact path='/' component={Home} />
-          <Route path='/shop' component={Shop} />
-          <Route path='/cart' component={Cart} />
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/shop" component={Shop} />
+          <Route path="/cart" component={Cart} />
         </Switch>
       </Router>
+      <div>
+        <Pictures cart={cart} addCart={addCart} />
+      </div>
     </>
   );
 }
