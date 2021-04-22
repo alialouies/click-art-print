b2a511376c4c95a031ac15ad66dc28c75d99d7f9
Angel Sámano Hamud <Angelsam@air-de-angel.localdomain>
2021-04-22 09:54:47 +0200

    First Form without hook

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,7 +12,7 @@ function Form(props) {
 
   const submitButton = (event) => {
    event.preventDefault();
-   window.confirm ("We received your order");
+   window.confirm (`We received your order`);
 }
 
   return (
@@ -20,55 +20,54 @@ function Form(props) {
 
     <form onSubmit={submitButton} className="form">
         <div className="form1">
-            
+        <div className="box">   
       <input 
         value={firstName}
         onChange={e => setFirstName(e.target.value)}
         placeholder="First name"
         type="text"
-        name="firstName"
-        
-        />
+        name="firstName"/>
+        </div>
+        <div className="box">
        <input 
         value={lastName}
         onChange={e => setLastName(e.target.value)}
         placeholder="Last name"
         type="text"
-        name="lastName"
-      />
+        name="lastName"/>
+        </div>
+        <div className="box">
       <input 
         value={email}
         onChange={e => setEmail(e.target.value)}
         placeholder="Email address"
         type="email"
-        name="email"
-       
-      />
+        name="email"/>
+        </div>
+        <div className="box">
       <input 
         value={street}
         onChange={e => setStreet(e.target.value)}
         placeholder="Street"
         type="Street"
-        name="Street"
-       
-      /> 
+        name="Street"/>
+        </div>
+        <div className="box"> 
  <input 
         value={zip}
         onChange={e => setZip(e.target.value)}
         placeholder="Zip Code "
         type="Zip Code"
-        name="Zip Code"
-       
-      />
+        name="Zip Code"/>
+        </div>
+        <div className="box">
       <input 
         value={city}
         onChange={e => setCity(e.target.value)}
         placeholder="City "
         type="City"
-        name="City"
-       
-      />
-
+        name="City"/>
+</div>
       <button type="submit" >Submit</button>
       </div>
     </form>
