ae7fcea75f602d2f02f52d1a0a035d1021240328
Maya Imhoff <mayelyimhoff@gmail.com>
2021-04-23 11:14:02 +0200

    added thank you page

---
+++ src/components/ThankYou.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../css/PrintArt.css"
+
+function ThankYou() {
+    return (
+        <>
+            <div className="intro">
+                <h1>Thank you, Anne!</h1>
+                <h2>Your order has been placed and will be shipped to asap!</h2>
+            </div>
+        </>
+    )
+}
+
+export default ThankYou
