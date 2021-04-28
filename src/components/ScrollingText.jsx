df203f2010a3c1bcaa1598f9344c87f39f3412da
alialouies <makarona.aliki@gmail.com>
2021-04-28 16:04:54 +0200

    adding scrolling-text component

---
+++ src/components/ScrollingText.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../css/ScrollingText.css';
+
+function ScrollingText() {
+  return (
+    <div>
+      <section id="hero">
+        <div className="marquee">
+          <p>CLICK ART PRINT</p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default ScrollingText;
