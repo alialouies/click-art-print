e2f81ab4e82b2a002cd3f983af7e90972b956ab6
EugeniuZaharia <78173175+EugeniuZaharia@users.noreply.github.com>
2021-05-07 10:29:39 +0200

    Met away and Shop MarginTop

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,10 @@ import Cart from "./components/pages/Cart";
 import Navbar from "./components/Navbar";
 import Checkout from "./components/pages/Checkout";
 import React, { useState, useEffect } from "react";
-import Purchase from './components/pages/Purchase'
-import Footer from "./components/Footer"
-import OurMission from "./components/OurMission"
-import Form from "./components/pages/Form"
-
+import Purchase from "./components/pages/Purchase";
+import Footer from "./components/Footer";
+import OurMission from "./components/OurMission";
+import Form from "./components/pages/Form";
 
 function App() {
   const [cart, setCart] = React.useState([]);
@@ -19,8 +18,8 @@ function App() {
   const addCart = (product, e) => {
     let tempState = [...cart];
     e.preventDefault();
-    tempState.push(product)
-    setCart(tempState)
+    tempState.push(product);
+    setCart(tempState);
     console.log(tempState);
   };
 
@@ -28,67 +27,79 @@ function App() {
     let tempProducts = [...cart];
     tempProducts.splice(index, 1);
     setCart(tempProducts);
-  }
+  };
 
   useEffect(() => {
-    const data = localStorage.getItem('shownCart');
+    const data = localStorage.getItem("shownCart");
     if (data) {
-      setCart(JSON.parse(data))
+      setCart(JSON.parse(data));
     }
-    },[])
+  }, []);
 
   useEffect(() => {
-    localStorage.setItem('shownCart', JSON.stringify(cart))
-    },[cart])
+    localStorage.setItem("shownCart", JSON.stringify(cart));
+  }, [cart]);
 
   const deleteStorage = () => {
     localStorage.removeItem("shownCart");
-  }
-
+  };
 
   const [state, setState] = React.useState({
-      firstName: "",
-      lastName: "",
-      email: "",
-      address: "",
-      zipCode: "",
-      creditCardNumber: "",
-      expirationDate: "",
-      cvv: ""
-  })
+    firstName: "",
+    lastName: "",
+    email: "",
+    address: "",
+    zipCode: "",
+    creditCardNumber: "",
+    expirationDate: "",
+    cvv: "",
+  });
 
   const handleChange = (evt) => {
     const value = evt.target.value;
     setState({
       ...state,
-      [evt.target.name]: value
+      [evt.target.name]: value,
     });
-    console.log(value)
-  }
-
+    console.log(value);
+  };
 
   return (
     <>
-    <div id="main">
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/"><Home/></Route>
-          <Route path="/shop"><Shop addCart={addCart} price={price}/></Route>
-          <Route path="/cart"><Cart cart={cart} price={price} deleteProduct={deleteProduct}/></Route>
-          <Route path="/checkout"><Checkout state={state} handleChange={handleChange} deleteStorage={deleteStorage}/></Route>
-          <Route path="/purchase"><Purchase state={state}/></Route>
-          <Route path="/footer1"><Footer/></Route>
-          <Route path="/form"><Form/></Route>
-          <Route path="/checkout"><Checkout /></Route>
-          <Route path="/purchase"><Purchase/></Route>
-        </Switch>
-        <Switch>
-        <Route path="/ourmission"><OurMission /></Route>
-        <Route path="/form"><Form/></Route>
-      </Switch>
-      < Footer />
-      </Router>
+      <div id="main">
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/shop">
+              <Shop addCart={addCart} price={price} />
+            </Route>
+            <Route path="/cart">
+              <Cart cart={cart} price={price} deleteProduct={deleteProduct} />
+            </Route>
+            <Route path="/checkout">
+              <Checkout
+                state={state}
+                handleChange={handleChange}
+                deleteStorage={deleteStorage}
+              />
+            </Route>
+            <Route path="/purchase">
+              <Purchase state={state} />
+            </Route>
+          </Switch>
+          <Switch>
+            <Route path="/ourmission">
+              <OurMission />
+            </Route>
+            <Route path="/form">
+              <Form />
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
       </div>
     </>
   );
